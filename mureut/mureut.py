9a0a018529f10fdb7ff7cf2edd00f14a617f9455
Me_Goes_RAWR <sammylover26@gmail.com>
2017-05-20 12:15:50 -0400

    Update mureut.py

--- mureut/mureut.py
+++ mureut/mureut.py
@@ -18,7 +18,11 @@ class MureUT:
                 await self.bot.say("```Total Steam Players online: " + data['services']['online']['title'] + "\n" +
                 "Steam Store Status: " + data['services']['store']['title'] + "\n" +
                 "Steam Community Status: " + data['services']['community']['title'] + "\n" + 
-                "Steam Database Status: " + data['services']['database']['title'] + "```")
+                "Steam WebAPI Status: " + data['services']['webapi']['title'] + "\n" + 
+                "CSGO Status: " + data['services']['csgo']['title'] + "\n" + 
+                "CSGO Inventory Status: " + data['services']['csgo_community']['title'] + "\n" + 
+                "CSGO Sessions Logon Status: " + data['services']['csgo_sessions']['title'] + "\n" + 
+                "For more info on Steam's Status: https://steamstat.us/\n" + "```")
 
 
 def setup(bot):
