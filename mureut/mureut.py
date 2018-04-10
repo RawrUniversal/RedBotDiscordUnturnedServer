4f9a08a40a233fea2587c9994ecd5e6384e4d701
Me_Goes_RAWR <sammylover26@gmail.com>
2018-04-10 16:07:33 -0400

    Update mureut.py

--- mureut/mureut.py
+++ mureut/mureut.py
@@ -139,7 +139,7 @@ class MureUT:
 
         return em
     
-    async def logs_menu(self, ctx, log,
+    def logs_menu(self, ctx, log,
                            message: discord.Message=None,
                            num=0, timeout: int=30):
         """menu control logic for this taken from
