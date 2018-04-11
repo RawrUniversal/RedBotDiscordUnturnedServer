42ad93fc007e8811040a8bb8a851e8d17907ab22
Me_Goes_RAWR <sammylover26@gmail.com>
2018-04-11 12:15:50 -0400

    Update mureut.py

--- mureut/mureut.py
+++ mureut/mureut.py
@@ -183,13 +183,14 @@ class MureUT:
                            message: discord.Message=None,
                            page=0, timeout: int=30):
         """menu control logic for this taken from
-            https://github.com/Lunar-Dust/Dusty-Cogs/blob/master/menu/menu.py"""\
-        server = ctx.message.author.server.id
+           https://github.com/Lunar-Dust/Dusty-Cogs/blob/master/menu/menu.py"""
+        author = ctx.message.author
+        server = author.server
         channel = cid
         if cid == 0:
             channel = ctx.message.channel.id
         base = os.path.join("data", "gnu")
-        server = os.path.join(base, str(server))
+        server = os.path.join(base, str(server.id))
         file = os.path.join(server, str(channel))
         text_file = open(file, "r")
         lines = text_file.read().split('\n')
