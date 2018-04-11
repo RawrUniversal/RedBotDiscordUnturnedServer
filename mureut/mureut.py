b5b07574597cb3cb62874c8afc453604710feaf3
Me_Goes_RAWR <sammylover26@gmail.com>
2018-04-11 14:32:00 -0400

    Update mureut.py

--- mureut/mureut.py
+++ mureut/mureut.py
@@ -196,7 +196,7 @@ class MureUT:
                 log += meh + "\n"
         if log.count('\n') == 0:
             return await self.bot.say("Nothing Found!")
-        logs = numpy.split(numpy.array(log.split('\n')), [1,2,3])
+        logs = numpy.split(numpy.array(log.split('\n')), 1)
         em = Embed(color=0x00F4FF,
                    title='Logs for {}'.format(info))
         em.add_field(name="Logs", value=str(logs[int(page)]))
