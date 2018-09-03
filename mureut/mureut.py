34614b28e1ef24e54077aa1d1b0ed6ca91223a98
Me_Goes_RAWR <sammylover26@gmail.com>
2018-09-03 14:50:44 -0400

    Update mureut.py

--- mureut/mureut.py
+++ mureut/mureut.py
@@ -276,7 +276,7 @@ class MureUT:
         em = Embed(color=0x00F4FF,
                    title='Logs for {}'.format(info))
         try:
-            em.add_field(name="Logs", value=str(logs[int(page)]))
+            em.add_field(name="Logs", value=str(logs[int(page)], encoding="utf8"))
         except IndexError:
             return await self.bot.say("Nothing Found!")
         if not message:
