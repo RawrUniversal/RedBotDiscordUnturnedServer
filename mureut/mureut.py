0e565f3bc47a29ae814183293a30937aefb79fde
Me_Goes_RAWR <sammylover26@gmail.com>
2018-09-08 11:52:14 -0400

    Update mureut.py

--- mureut/mureut.py
+++ mureut/mureut.py
@@ -37,24 +37,11 @@ class MureUT:
             key = jdata['key']
         self.token = key  #  set this to your DBL token
         self.dblpy = dbl.Client(self.bot, self.token)
-        self.bot.loop.create_task(self.update_stats())
         
     def chunks(s, n):
         for start in range(0, len(s), n):
             yield s[start:start+n]
-
-    async def update_stats(self):
-        """This function runs every 30 minutes to automatically update your server count"""
-
-        while True:
-            logger.info('attempting to post server count')
-            try:
-                await self.dblpy.post_server_count()
-                logger.info('posted server count ({})'.format(len(self.bot.guilds)))
-            except Exception as e:
-                logger.exception('Failed to post server count\n{}: {}'.format(type(e).__name__, e))
-            await asyncio.sleep(1800)
-            
+           
             
     @commands.command()
     async def steamstatus(self):
