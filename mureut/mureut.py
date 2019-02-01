9c3e602fb05bcf5b9393d580ef49629de25b0e30
Me_Goes_RAWR <sammylover26@gmail.com>
2019-02-01 15:50:39 -0500

    Update mureut.py

--- mureut/mureut.py
+++ mureut/mureut.py
@@ -181,6 +181,14 @@ class MureUT:
         item = False
         return item
 
+    async def listener(self, message):
+        channel = message.channel
+        if message.author.id != self.bot.user.id:
+            if 'thot' in message.content.lower():
+                await self.bot.send_message(message.channel, 'thot-bot*')
+            if message.content.lower().startswith('you a thot') or 'thot-bot' in message.content.lower():
+                await self.bot.send_message(message.channel, 'no! you a thot!')
+    
     def request_item_json_osbuddy(item):
         with urllib.request.urlopen("https://storage.googleapis.com/osbuddy-exchange/summary.json") as response:
             item_info = simplejson.load(response)
@@ -349,4 +357,6 @@ class MureUT:
 def setup(bot):
     global logger
     logger = logging.getLogger('bot')
-    bot.add_cog(MureUT(bot))
+    n = MureUT(bot)
+    bot.add_listener(n.listener, "on_message")
+    bot.add_cog(n)
