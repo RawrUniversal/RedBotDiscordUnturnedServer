b79bee2bddd1766211c6061f8393d1a87518c64b
Me_Goes_RAWR <sammylover26@gmail.com>
2019-05-05 16:44:10 -0400

    Update mureut.py

--- mureut/mureut.py
+++ mureut/mureut.py
@@ -49,7 +49,7 @@ class MureUT:
         if itemorveh == "item":
             with open(config_path, encoding="utf-8") as item_ids:
                 jdata = json.load(item_ids)
-                item = MureUT.check_unturned(json, self)
+                item = MureUT.check_unturned(json)
                 if item is False:
                     await self.bot.say("That item doesn't exist!")
                     return
