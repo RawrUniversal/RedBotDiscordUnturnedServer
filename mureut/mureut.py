6f52b6bab14cd7dfdc4fe95340369dbc7981b125
Me_Goes_RAWR <sammylover26@gmail.com>
2019-05-13 13:58:37 -0400

    Update mureut.py

--- mureut/mureut.py
+++ mureut/mureut.py
@@ -355,7 +355,7 @@ class MureUT:
         if log.count('\n') == 0:
             return await self.bot.say("Nothing found!")
         logs = list(MureUT.chunks(log.split('|'), 10))
-        log = logs[int(page)] + '|'
+        log = ''.join(logs[int(page)]) + '|'
         em = Embed(color=0x00F4FF,
                    title="Logs pages: {}/{} | Don't go too far foward or back!".format(page + 1, len(logs)), timestamp=datetime.now())
         try:
